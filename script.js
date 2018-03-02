c20743743b5c42589b709534c1c42c95a252e8d8
ramonemunoz <ramonmunoz1027@gmail.com>
2018-03-01 21:26:20 -0800

    basic recursion functions

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function factorial(num) {
 }
 wr('Factorial');
 wr(factorial(5));
+wr();
+
+function basicRecursion(max, current) {
+	if (current > max) return;
+	wr(current);
+	basicRecursion(max, current + 1);
+}
+wr('Basic Recusrion');
+basicRecursion(5, 1);
+wr();
+
+function fibonacci(n) {
+	if (n <= 2) {
+		return 1;
+	} else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
+
+wr('Fibonacci');
+for (var i = 1; i <= 20; i++) {
+	wr(`${i} | ${fibonacci(i)}`);
+}
+wr();
