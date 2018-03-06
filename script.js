cf1f0022181590bfa57f0f1a21d89a83fe0d7f1b
ramonemunoz <ramonmunoz1027@gmail.com>
2018-03-05 21:44:21 -0800

    added bubble sort and insertion sort

--- script.js
+++ script.js
@@ -30,3 +30,38 @@ for (var i = 1; i <= 20; i++) {
 	wr(`${i} | ${fibonacci(i)}`);
 }
 wr();
+
+wr('Bubble Sort');
+var bubbleSort = nums => {
+	do {
+		var swapped = false;
+		for (var i = 0; i < nums.length; i++) {
+			if (nums[i] > nums[i + 1]) {
+				var temp = nums[i];
+				nums[i] = nums[i + 1];
+				nums[i + 1] = temp;
+				swapped = true;
+			}
+		}
+	} while (swapped);
+	return nums;
+};
+var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+wr(bubbleSort(nums));
+wr();
+
+wr('Insertion Sort');
+var insertionSort = nums => {
+	for (let i = 1; i < nums.length; i++) {
+		for (let j = 0; j < i; j++) {
+			if (nums[i] < nums[j]) {
+				let spliced = nums.splice(i, 1);
+				nums.splice(j, 0, spliced[0]);
+			}
+		}
+	}
+	return nums;
+};
+var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+wr(insertionSort(nums));
+wr();
