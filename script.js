4de40a427656cd479e1be6eb4079f0e8f640eedb
ramonemunoz <ramonmunoz1027@gmail.com>
2018-03-06 22:04:04 -0800

    added merge sort

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ var bubbleSort = nums => {
 	return nums;
 };
 var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+wr(nums);
 wr(bubbleSort(nums));
 wr();
 
@@ -63,5 +64,38 @@ var insertionSort = nums => {
 	return nums;
 };
 var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+wr(nums);
 wr(insertionSort(nums));
+wr();
+
+wr('Merge Sort');
+const mergeSort = nums => {
+	if (nums.length < 2) {
+		return nums;
+	}
+	const length = nums.length;
+	const middle = Math.floor(length / 2);
+	const left = nums.slice(0, middle);
+	const right = nums.slice(middle);
+
+	return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left, right) => {
+	const results = [];
+
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+	}
+
+	return results.concat(left, right);
+};
+
+var nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+wr(nums);
+wr(mergeSort(nums));
 wr();
